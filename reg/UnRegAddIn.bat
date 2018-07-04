49b3d4f81ca3f7bd71891311005f89fbf0dff8aa
djserega <galyuk.sergey@gmail.com>
2018-07-04 09:49:19 +0300

    initial project

---
+++ reg/UnRegAddIn.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+REM The following directory is for .NET 4.0
+set DOTNETFX4=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
+set PATH=%PATH%;%DOTNETFX4%
+echo ---------------------------------------------------
+regasm.exe "%~dp0..\src\AddInScannerBarcode\bin\Release\AddIn.ScannerBarcode.dll" /unregister
+echo ---------------------------------------------------
+pause
---
+++ src/AddInScannerBarcode/IScannerBarcode.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AddIn
+{
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IScannerBarcode
+    {
+        bool Connected { get; }
+        string Error { get; }
+        int Port { get; set; }
+
+        bool Connect();
+        void Disconnect();
+    }
+}
